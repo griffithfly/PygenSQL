8dd0a7b84c73414003c596440831b52e0bf378c0
griffith <131559221+griffithfly@users.noreply.github.com>
2023-04-24 17:35:10 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,28 @@
 # Test Module 1
 
 import Creat_SQL_table as ct
-
+'''
+'your_data_dictionary.xlsx' is a data dictionary file. 
+A standard example(example_data_dictionary.xlsx) was provide in this repository.
+'''
 file = ct.Create_Table()
 loc = file.get_fields('your_data_dictionary.xlsx')
 fields = file.genquery_table_dic('your_data_dictionary.xlsx')
 print(fields)
+
+## For example, copy & paste following code to your local. Please make sure 'example_data_dictionary.xlsx' in the same directory.
+
+import pandas as pd
+import Creat_SQL_table as ct
+unit_test = pd.read_excel('example_data_dictionary.xlsx')
+file = ct.Create_Table()
+loc = file.get_fields('example_data_dictionary.xlsx')
+fields = file.genquery_table_dic('example_data_dictionary.xlsx')
+
+print(fields, len(unit_test)) # Output: (['visitid [Done]', 'SYS_LOC_CODE [Done]', 'marital_status [Done]', 'conc [Done]', 'analyte [Done]'], 5) 5
+## A .txt file will be created in the same directory containing SQL queries for creating a table.
+## The First 5 is 5 fields were successfully processed. The second 5 is the length of fields in the data dictionary file.
+## Numbers are the same means all variables are processed.
 
 
 # Test Module 2
@@ -15,6 +32,8 @@ import Data_Validation as dv
 files = dv.Check_Table('your_data_dictionary.csv', 'your_data_export.csv').check_fields()
 report = dv.Check_Table('your_data_dictionary.csv', 'your_data_export.csv').field_report()
 report = dv.Check_Table('your_data_dictionary.csv', 'your_data_export.csv').logical_report()
+
+## A error log (.txt file) will be created in the same directory containing CODE
 
 # Test Module 3
 
